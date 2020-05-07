Fixed a bug which caused maps generated by the RTT generator to be one pixel wider and taller than specified. Fixed a bug which caused maps to be saved with floating point precision.

baselines_lab/env/gym_maze/maze_generators/maze_generator.py
@@ -23,7 +23,7 @@ class InstanceGenerator(ABC):
         return [seed]
 
     def save(self, path) -> None:
-        np.savetxt(path, self._last)
+        np.savetxt(path, self._last, fmt="%d")
 
     def last(self) -> np.ndarray:
         return self._last

baselines_lab/env/gym_maze/maze_generators/rrt_generator.py
@@ -50,15 +50,15 @@ class RRTGenerator(InstanceGenerator):
         self.border = border
 
         if self.border:
-            self.width -= 1
-            self.height -= 1
+            self.width -= 2
+            self.height -= 2
 
     def generate(self) -> np.ndarray:
         nodes = self._generate_tree()
         self._calculate_flow(nodes)
         self._create_loops(nodes)
         maze = self._draw_graph(nodes)
-        self._last = self._create_border(maze)
+        self._last = self._create_border(maze).astype(np.uint8)
         return self._last
 
     def _generate_tree(self) -> List[Node]: