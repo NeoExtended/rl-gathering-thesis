8724092796438d084be018eae643aa9507688ebf
NeoExtended <wirspielen@web.de>
2020-06-16 21:28:16 +0200

    Added an option to propagate reward as observation.

--- baselines_lab/env/wrappers/wrappers.py
+++ baselines_lab/env/wrappers/wrappers.py
@@ -32,23 +32,34 @@ class WarpGrayscaleFrame(gym.ObservationWrapper):
 
 
 class NoObsWrapper(gym.Wrapper):
-    def __init__(self, env):
+    def __init__(self, env, rew_is_obs=False):
         """
         Deletes the observation from the env and replaces it with a counter which increases with each step.
         """
         gym.Wrapper.__init__(self, env)
         self.counter = np.array([0])
-        self.observation_space = gym.spaces.Box(low=0, high=np.inf, shape=(1,), dtype=int)
+        self.rew_is_obs = rew_is_obs
+        if rew_is_obs:
+            self.observation_space = gym.spaces.Box(low=0, high=np.inf, shape=(2,), dtype=int)
+        else:
+            self.observation_space = gym.spaces.Box(low=0, high=np.inf, shape=(1,), dtype=int)
 
     def step(self, action):
         obs, rew, done, info = super(NoObsWrapper, self).step(action)
         self.counter[0] += 1
-        return self.counter, rew, done, info
+        if self.rew_is_obs:
+            return np.array(self.counter[0], rew), rew, done, info
+        else:
+            return self.counter, rew, done, info
 
     def reset(self, **kwargs):
         super(NoObsWrapper, self).reset(**kwargs)
         self.counter[0] = 0
-        return self.counter
+
+        if self.rew_is_obs:
+            return np.array([self.counter[0], 0])
+        else:
+            return self.counter
 
 
 class VecGifRecorder(VecEnvWrapper):
