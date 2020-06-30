53bdad43436bd03c0456f46ac4297af0a1868b13
NeoExtended <wirspielen@web.de>
2020-06-30 23:59:29 +0200

    Small fix.

--- baselines_lab/utils/config_util.py
+++ baselines_lab/utils/config_util.py
@@ -26,6 +26,7 @@ def parse_config_args(config_args, args):
         assert os.path.exists(config), "Invalid input file/directory {}".format(config)
         if os.path.isdir(config):
             files = glob.glob(os.path.join(config, "**/*.yml"))
+            files.extend(os.path.join(config, "*.yml"))
             for file in files:
                 configs.append(get_config(file, args))
         else:
