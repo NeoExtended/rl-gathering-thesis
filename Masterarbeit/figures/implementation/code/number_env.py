101a091df0000cba920d654ae0195fa2a26a3a43
NeoExtended <wirspielen@web.de>
2020-07-06 19:56:50 +0200

    Added usage example for Baselines Lab. Added evaluation of physical particle models. Updated PDF.

--- Masterarbeit/figures/implementation/code/number_env.py
+++ Masterarbeit/figures/implementation/code/number_env.py
@@ -7,12 +7,12 @@ class NumberEnv(gym.Env):
         # Define actions and observations
         self.action_map = {0: 1, 1: 5, 2: 10, 3: -1, 4: -5, 5: -10}
         self.action_space = gym.spaces.Discrete(len(self.action_map))
-        self.observation_space = gym.spaces.Box(low=0, high=max_num, shape=(2,), dtype=int)
-
+        self.observation_space = gym.spaces.Box(low=0, high=max_num,
+                                                shape=(2,), dtype=int)
         # Initialize variables
         self.np_random = np.random.default_rng()
-        self.current_number = 0
-        self.target_number = 0
+        self.current = 0
+        self.target = 0
         self.max_num = max_num
 
     def seed(self, seed=None):
@@ -20,21 +20,19 @@ class NumberEnv(gym.Env):
         return [seed]
 
     def reset(self):
-        self.current_number = np.random.randint(0, self.max_num)
-        self.target_number = np.random.randint(0, self.max_num)
+        self.current = np.random.randint(0, self.max_num)
+        self.target = np.random.randint(0, self.max_num)
         return self._make_observation()
 
     def step(self, action):
-        done = False
-        new_number = self.current_number + self.action_map[action]
-        reward = abs(self.current_number - self.target_number) - abs(self.target_number - new_number)
-        self.current_number = new_number
-        if self.current_number == self.target_number:
-            done = True
+        new = self.current + self.action_map[action]
+        reward = -0.5 + abs(self.current - self.target) - abs(self.target - new)
+        self.current = new
+        done = True if self.current == self.target else False
         return self._make_observation(), reward, done, {}
 
     def _make_observation(self) -> np.ndarray:
-        return np.array([self.current_number, self.target_number])
+        return np.array([self.current, self.target])
 
     def render(self, mode='human'):
         pass
